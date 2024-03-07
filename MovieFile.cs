bd5e0f40a536fef84a607624c64810d006a03983
Braedon Fredin <b260fredin@gmail.com>
2024-03-07 14:18:34 -0600

    [1.13] Verify unique movie title

--- MovieFile.cs
+++ MovieFile.cs
@@ -6,15 +6,13 @@ public class MovieFile
   // public property
   public string filePath { get; set; }
   public List<Movie> Movies { get; set; }
+  private static NLog.Logger logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
   // constructor is a special method that is invoked
   // when an instance of a class is created
   public MovieFile(string movieFilePath)
   {
     filePath = movieFilePath;
-       // create instance of Logger
-    NLog.Logger logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
-
     Movies = new List<Movie>();
 
     // to populate the list with data, read from the data file
